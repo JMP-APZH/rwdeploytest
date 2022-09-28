0d3b2e699efbeaa459290b1b36a416dc822c9e20
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2022-09-28 13:15:04 +0200

    homepage prep

---
+++ rwdeploytest/web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,24 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+
+      <h1 className='bg-black text-blue-400 text-center'>HomePage</h1>
+
+      <div className='h-screen flex flex-col text-center place-content-center bg-blue-300'>
+        <p>
+          Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
+        </p>
+        <p>
+          My default route is named <code>home</code>, link to me with `
+          <Link to={routes.home()}>Home</Link>`
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default HomePage
